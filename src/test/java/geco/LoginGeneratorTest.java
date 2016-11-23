54e281570c157597b3e3fa1d91f5c41ab5b39052
Cloudinet <luffy.cloud@hotmail.fr>
2016-11-23 10:48:17 +0100

    #3 test

--- src/test/java/geco/LoginGeneratorTest.java
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -6,7 +6,7 @@ public class LoginGeneratorTest {
 
     @Test
     public void testGenerateLoginForNomAndPrenom() throws Exception {
-        String[] s = {"liste"};
+        //String[] s = {"liste"};
         LoginService test = new LoginService(new String[] {"JROL", "BPER",
                 "CGUR", "JDUP", "JRAL", "JRAL1"});
         LoginGenerator lg = new LoginGenerator(test);
